5bb8a17ab2ed26b275a72f02156501f4b213f41f
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-10-28 23:13:04 -0300

    Initial config

---
+++ configuration.nix
@@ -0,0 +1,155 @@
+{ config, pkgs, ... }:
+
+let
+  starRailTheme = pkgs.fetchFromGitHub {
+    owner = "voidlhf";
+    repo = "StarRailGrubThemes";
+    rev = "20250927-065308";
+    sha256 = "sha256-UfFDYB6VKMF5OPoDSNDhzEoC1EDcpC34C+ebQvjLuvU=";
+  };
+in
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    theme = "${starRailTheme}/assets/themes/Evernight";
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "nixos";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
+
+  console.keyMap = "br-abnt2";
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.matheus = {
+    isNormalUser = true;
+    description = "matheus";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+  };
+  home-manager.users.matheus = {
+    imports = [ ./home.nix ];
+  };
+
+  home-manager.backupFileExtension = "backup";
+
+  programs.firefox.enable = true;
+
+  virtualisation.docker = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    fish
+    tmux
+
+    neofetch
+    fastfetch
+
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.user-themes
+    gnomeExtensions.just-perfection
+    gnomeExtensions.open-bar
+    gnomeExtensions.logo-menu
+    gnomeExtensions.quick-settings-tweaker
+    gnomeExtensions.freon
+    tokyonight-gtk-theme
+    papirus-icon-theme
+
+    lm_sensors
+    lazydocker
+    git
+    vscode
+    discord
+    btop
+
+    # Para desenvolvimento
+    nodejs_22
+    terraform
+    terraformer
+    tfswitch
+    nodePackages_latest.ts-node
+    awscli2
+    python314
+
+    # Para estudos
+    obsidian
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    geary
+    gnome-contacts
+    gnome-weather
+    gnome-clocks
+    gnome-maps
+    gnome-music
+    totem
+    gnome-calendar
+    gnome-tour
+    epiphany
+    yelp
+    gnome-connections
+    snapshot
+    simple-scan
+    baobab
+    gnome-logs
+  ];
+
+  system.stateVersion = "25.05";
+}
---
+++ home.nix
@@ -0,0 +1,219 @@
+{ configs, pkgs, ... }: 
+
+let
+  evernightWallpaper = pkgs.fetchurl {
+    url = "https://4kwallpapers.com/images/wallpapers/evernight-honkai-3840x2160-24123.jpg";
+    sha256 = "3kS9bmlBdjsSP3kDNTaPiPe3CiODgU+2VAy57YLlvbQ=";
+  };
+in
+{
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [];
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      icon-theme = "Papirus-Dark";
+      gtk-theme = "Tokyonight-Dark";
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Tokyonight-Dark";
+    };
+
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "kitty.desktop"
+        "org.gnome.Nautilus.desktop"
+        "discord.desktop"
+        "code.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${evernightWallpaper}";
+      picture-uri-dark = "file://${evernightWallpaper}";
+      picture-options = "zoomed";
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set -x fish_greeting ""
+    '';
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      # QOF
+      shell = "tmux new-session -A -s main";
+      confirm_os_window_close = 0;
+      initial_window_width = 800;
+      initial_window_height = 600;
+      remember_window_size = "yes";
+    
+      # Fontes
+      font_size = 12.0;
+
+      # Background Transparente
+      url_color = "#d65c9d";
+      background_opacity = 0.75;
+      dynamic_background_opacity = "yes";
+
+      # Cores
+      background           = "#000000";
+      foreground           = "#afc2c2";
+      cursor               = "#ffffff";
+      selection_background = "#7cbeff";
+      color0               = "#bbcbcc";
+      color8               = "#ffffff";
+      color1               = "#ff2f2f";
+      color9               = "#ff2f2f";
+      color2               = "#549a6f";
+      color10              = "#549a6f";
+      color3               = "#ccac00";
+      color11              = "#ccac00";
+      color4               = "#0099cc";
+      color12              = "#0099cc";
+      color5               = "#cc68c8";
+      color13              = "#cc68c8";
+      color6               = "#79c4cc";
+      color14              = "#79c4cc";
+      color7               = "#000000";
+      color15              = "#000000";
+      selection_foreground = "#000000";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      set-option -g default-shell ${pkgs.fish}/bin/fish
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+
+    userName = "Matheus-Merlos";
+    userEmail = "matheusazrmerlos@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "master";
+      pull.rebase = false;
+      core.editor = "code --wait";
+      push.autoSetupRemote = true;
+    };
+  };
+
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      display = {
+        separator = " ➜  ";
+      };
+
+      modules = [
+        "break"
+        "break"
+        "break"
+        {
+          type = "os";
+          key = "OS   ";
+          keyColor = "36";
+        }
+        {
+          type = "kernel";
+          key = " ├  ";
+          keyColor = "36";
+        }
+        {
+          type = "packages";
+          format = "{} (NixOS)"; 
+          key = " ├ 󰏖 ";
+          keyColor = "36";
+        }
+        {
+          type = "shell";
+          key = " └  ";
+          keyColor = "36";
+        }
+        "break"
+        {
+          type = "wm";
+          key = "WM   ";
+          keyColor = "34";
+        }
+        {
+          type = "wmtheme";
+          key = " ├ 󰉼 ";
+          keyColor = "34";
+        }
+        {
+          type = "icons";
+          key = " ├ 󰀻 ";
+          keyColor = "34";
+        }
+        {
+          type = "cursor";
+          key = " ├  ";
+          keyColor = "34";
+        }
+        {
+          type = "terminal";
+          key = " ├  ";
+          keyColor = "34";
+        }
+        {
+          type = "terminalfont";
+          key = " └  ";
+          keyColor = "34";
+        }
+        "break"
+        {
+          type = "host";
+          format = "{5} {1} Type {2}";
+          key = "PC   ";
+          keyColor = "35";
+        }
+        {
+          type = "cpu";
+          format = "{1} ({3}) @ {7} GHz";
+          key = " ├  ";
+          keyColor = "35";
+        }
+        {
+          type = "gpu";
+          format = "{1} {2} @ {12} GHz";
+          key = " ├ 󰢮 ";
+          keyColor = "35";
+        }
+        {
+          type = "memory";
+          key = " ├  ";
+          keyColor = "35";
+        }
+        {
+          type = "swap";
+          key = " ├ 󰓡 ";
+          keyColor = "35";
+        }
+        {
+          type = "disk";
+          key = " ├ 󰋊 ";
+          keyColor = "35";
+        }
+        {
+          type = "monitor";
+          key = " └ 󰋊 ";
+          keyColor = "35";
+        }
+        "break"
+        "break"
+      ];
+    };
+  };
+}
